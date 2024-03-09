fa33d24bd5e2d20ff9f67f79b00f09c708796a51
Lucas Souza <lucassouzafranco1@gmail.com>
2024-03-09 09:20:57 -0300

    ✨ feat(react-icons): adds icons & structure for insert links

--- src/assets/components/Menu/Menu.tsx
+++ src/assets/components/Menu/Menu.tsx
@@ -1,8 +1,25 @@
 import React from "react";
+import './Menu.css';
+import { RiOpenaiFill } from "react-icons/ri";
+import { RxQuestionMarkCircled } from "react-icons/rx";
+import { FaGithub } from "react-icons/fa";
+import { HiMiniMoon } from "react-icons/hi2";
 
 const Menu: React.FC = () => {
   return (
-    <div>MenuReact</div>
+    <div className="MenuContainer">
+      <div className="MenuContent">
+        <div className="IconsAtLeft">
+          <RxQuestionMarkCircled className="DoubtsIcon"/>
+          <RiOpenaiFill className="OpenAIcon" />
+        </div>
+        guess the prompt_
+        <div className="IconsAtRight">
+          <FaGithub className="GitHubIcon"/>
+          <HiMiniMoon className="DarkModeIcon"/>
+        </div>
+      </div>
+    </div>
   );
 };
 
